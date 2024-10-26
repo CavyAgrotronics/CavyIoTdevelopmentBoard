9dec06f2b19fcd0e7f3747916dc64bf9ee6da694
Cavy Agrotronics <44072182+CavyAgrotronics@users.noreply.github.com>
2024-10-26 09:41:03 +0530

    Update ESP8266-httpupdate.ino

--- ESP8266-httpupdate/ESP8266-httpupdate.ino
+++ ESP8266-httpupdate/ESP8266-httpupdate.ino
@@ -2,10 +2,14 @@
 "CavyIoT.nodemcu-V0.03.bin" 
 
 CavyIoTdevelopment board firmware download for IC ESP8266  and NodeMCU 1.0(ESP-12E Module)
+
+ESP8266 board 3.1.2 
 */
 #include <ESP8266httpUpdate.h>
+#include <WiFiClient.h>
 const char* ssid =     "WiFiSSID";     // Set your router SSID
 const char* password = "WiFiPassword"; // Set your router password
+WiFiClient client;
 void setup()
 {
   Serial.begin(9600);
@@ -15,7 +19,7 @@ void setup()
                        delay(500);
                                }
   Serial.print("Downloding.........Wait  2 to 3 minutes for update...");
-  t_httpUpdate_return ret = ESPhttpUpdate.update("http://www.developers.cavyiot.com/downloads/CavyIoT.nodemcu-V0.03.bin"); 
+  t_httpUpdate_return ret = ESPhttpUpdate.update(client,"http://www.developers.cavyiot.com/downloads/CavyIoT.nodemcu-V0.03.bin"); 
    switch (ret) {
     case HTTP_UPDATE_FAILED:
       Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
